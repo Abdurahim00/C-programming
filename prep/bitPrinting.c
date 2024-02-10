3d9a76740ea69e0977ef665ebbf1249a4a478899
Abdurahim00 <104921782+Abdurahim00@users.noreply.github.com>
2024-02-10 10:46:03 +0100

    ex b

--- prep/bitPrinting.c
+++ prep/bitPrinting.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
+#include <stdint.h>
 
+void print_bin(unsigned int bits)
+{
 
-
-void print_bin(unsigned int bits){
-
- for (int i = 0; i < 32; i++)
+    for (int i = 0; i < 32; i++)
     {
-        int bit = (bits>> (31-i)) & 1;
+        int bit = (bits >> (31 - i)) & 1;
         printf("%d", bit);
 
-    if(i%8 == 7 && i!=31){
-        printf(".");
+        if (i % 8 == 7 && i != 31)
+        {
+            printf(".");
+        }
     }
-        
-    }
-
 }
+
+uint16_t make_16bit(uint8_t least_significant, uint8_t
+                                                   most_significant)
+{
+       uint16_t most_right = most_significant << 8;
+       uint16_t sum = most_right | least_significant;
+
+       return sum;
+}
+
+
 int main(int argc, char const *argv[])
 {
 
-    
-   
     print_bin(43);
-
+    printf("\n");
+    print_bin(make_16bit(7, 1)); 
     return 0;
 }
