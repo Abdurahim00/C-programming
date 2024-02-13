e938c34fc29da3536636e4c965b982f9ae6f0dae
Abdurahim00 <104921782+Abdurahim00@users.noreply.github.com>
2024-02-13 09:16:48 +0100

    comments

--- WP3/ex6.c
+++ WP3/ex6.c
@@ -1,32 +1,36 @@
+// Include sections
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
 
+// Efficiently copies a string from str to copyOfstr
 void copyString(char *str, char *copyOfstr)
 {
-    for (int i = 0; i < strlen(str); i++)
-    {
+    int len = strlen(str); // Calculate length once for efficiency
+    // Copy each character
+    for (int i = 0; i < len; i++)
+    { // copy over to copyOfstr
         copyOfstr[i] = str[i];
-    }
+    } // Null-terminate the copied string
     copyOfstr[strlen(str)] = '\0';
 }
 
 int main(int argc, char const *argv[])
 {
 
-    char str[21];
-    char copyOfstr[21];
+    char str[21];       // initialize an array
+    char copyOfstr[21]; // another array for the copies
 
     printf("Enter a string: ");
-    scanf("%20s", str);
+    scanf("%20s", str); // input to the str array with 20 char
 
-    strcpy(copyOfstr, str);
+    strcpy(copyOfstr, str); // we copy over from str to copyOfstr array
     printf("Copied string: %s\n", copyOfstr);
 
     memset(copyOfstr, 0, sizeof(copyOfstr)); // reset for testing the copystring function
 
-    copyString(str, copyOfstr);
+    copyString(str, copyOfstr); // we copy from copyOfstr to str
     printf("Copied string (copyString function): %s\n", copyOfstr);
 
-    return 0;
+    return 0; // exit
 }
